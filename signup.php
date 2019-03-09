9e47be3a8d7f28ac03ccc38037638e0651b67444
TARS <ritvikjain99@gmail.com>
2019-03-09 19:24:18 +0530

    added html

--- signup.php
+++ signup.php
@@ -1,1 +1,23 @@
-dfadf
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href='signup-css.css' rel='stylesheet'>
+</head>
+<body>
+    <div class="signup-box">
+        <input type="text" placeholder="Name" name="sname">
+        <input type="text" placeholder="Email-Adress" name="semail">
+        <input type="password" placeholder="New Password" name="spassword">
+    </div>
+    <div class="login-box">
+        <input type="text" class="lusername" placeholder="Username">
+        <input type="password" class="lpassword" placeholder="Password">
+        <input type="submit" class="submit" name="lsubmit">
+    </div>
+</body>
+<script src="signup-js.js"></script>
+</html>
